576ef8388d8600d7b7479083746c81b5c8bf7116
David Mezza <d.mezza@zunocarbon.com>
2022-07-30 16:15:49 +0300

    :fire: Search Bar + Code Refactoring

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:medical_app/pages/widgets/home/search.dart';
+import 'package:medical_app/pages/widgets/home/topbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,69 +14,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor("ffffff"),
-      body: ListView(children: [
-        Container(
-          margin: const EdgeInsets.only(left: 15, right: 15, top: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //Column
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text("Hello"),
-                    Text(
-                      "Mr Elinonga 👋",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                      ),
-                    ),
-                  ]),
+      backgroundColor: HexColor("f3f8fb"),
+      body: ListView(children: const [
+        // Row (Salamu + Notification + Avatar)
+        TopBar(),
+        SizedBox(height: 30),
 
-              // Row (Notification + Avatar)
-              Row(
-                children: [
-                  Stack(children: [
-                    // Notification
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle, color: HexColor("e8f2f9")),
-                      child: const Icon(
-                        Icons.notifications,
-                        color: Colors.blue,
-                      ),
-                    ),
+        // Search bar
+        SearchBar(),
+        SizedBox(height: 30),
 
-                    // Red dot
-                    Positioned(
-                      right: 10,
-                      child: Container(
-                        width: 11,
-                        height: 11,
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.red),
-                      ),
-                    )
-                  ]),
-
-                  const SizedBox(
-                    width: 10,
-                  ),
-
-                  // Avatar
-                  const CircleAvatar(
-                    radius: 25,
-                    foregroundImage: AssetImage("assets/images/black.jpeg"),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
+        // My Appointments
       ]),
     );
   }
